7b2e04eda98dbaea3385c95faf95b3917015afd1
Nikita Prohorenko <chlorovodorod@gmail.com>
2026-08-20 14:56:30 +0300

    update p7t1.sql, перестал дублировать

--- HOMEWORK 3-SQL Introduction/p7t1.sql
+++ HOMEWORK 3-SQL Introduction/p7t1.sql
@@ -7,10 +7,15 @@ from
 	customers as c 
 join 
 	orders as o on c.customer_id = o.customer_id  
-join 
-	shippings as s ON c.customer_id = s.customer
-where 
-	s.status = 'Delivered'
+--join 
+	--shippings as s ON c.customer_id = s.customer
+where exists (
+    select 1
+    from shippings as s
+    where s.customer = c.customer_id
+      and s.status = 'Delivered'
+)
+--s.status = 'Delivered'
 group by 
 	c.customer_id, c.first_name, c.last_name, c.country
 having count(o.order_id) >= 2
